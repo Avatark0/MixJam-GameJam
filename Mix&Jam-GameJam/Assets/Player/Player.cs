36c92606bb4481c68cd7593e0429ee9ee5d0688f
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2020-11-14 12:10:58 -0300

    Integration test
    Testing git integration of versions with lustart (Colaborator).

--- Mix&Jam-GameJam/Assets/Player/Player.cs
+++ Mix&Jam-GameJam/Assets/Player/Player.cs
@@ -4,17 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator anim;
+    public float speed=0;
     void Start()
     {
-        
+        anim=GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.Space)){
-            
+            Debug.Log(speed);
+            anim.SetBool("isJumping",true);
+            transform.position += new Vector3(0, 0.1f, 0);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            speed+=0.2f;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            speed-=0.2f;
+        }
+        anim.SetFloat("speed",speed);
+    }
+    //verticalInput * movementSpeed * Time.deltaTime
+
+    void OnCollisionEnter(Collision col){
+        Debug.Log("Player colidiu com "+col);
+        if(col.gameObject.tag=="treadmill"){
+            anim.SetBool("isJumping",false);
         }
     }
 }
