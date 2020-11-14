4467fdefb49f247ee6fc20a41fa5f4d339928c94
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2020-11-14 17:49:31 -0300

    Player Animations, Scene division, Cleanup, Pivot Preefab
    Player Animations: added the main animations to the player character.
    
    Scene division: Divided the project into scenes for each colaborator, to solve merging errors.
    
    Cleanup: cleaned and organized the assets folder.
    
    Pivot Preefab: created the preefab for the canvas pivot mechanic.

---
+++ Mix&Jam-GameJam/Assets/Canvas/ButtonPivot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPivot : MonoBehaviour
+{
+    public float rotSpeed=1;
+    void Update()
+    {
+        Vector3 rotation=new Vector3(0,0,rotSpeed*Time.deltaTime);
+        transform.Rotate(rotation,Space.Self);
+    }
+}
--- Mix&Jam-GameJam/Assets/Player/Player.cs
+++ Mix&Jam-GameJam/Assets/Player/Player.cs
@@ -6,32 +6,58 @@ public class Player : MonoBehaviour
 {
     private Animator anim;
     public float speed=0;
-    void Start()
+
+    private Rigidbody rb;
+
+    public float jumpStrenght=3.4f;
+    private float jumpLenght=0.13f;//0.26 seconds total duration
+    private float timer;
+
+    void Awake()
     {
         anim=GetComponent<Animator>();
+        rb=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
-            Debug.Log(speed);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            timer=Time.time;
             anim.SetBool("isJumping",true);
-            transform.position += new Vector3(0, 0.1f, 0);
+            rb.velocity = new Vector3(0,jumpStrenght,0);
+            Debug.Log("Jump!");
         }
-        if(Input.GetKey(KeyCode.D)){
+        if(Input.GetKeyDown(KeyCode.E)){
+            jumpStrenght-=0.2f;
+            anim.SetBool("isSliding",true);
+            Debug.Log("Slide!");
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
             speed+=0.2f;
         }
-        if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKeyDown(KeyCode.A)){
             speed-=0.2f;
         }
+        if(Input.GetKeyDown(KeyCode.W)){
+            jumpStrenght+=0.2f;
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            jumpStrenght-=0.2f;
+        }
         anim.SetFloat("speed",speed);
+    }
+
+    void FixedUpdate(){
+        if(timer>Time.time-jumpLenght){
+            Debug.Log("Subindo");
+        }
     }
     //verticalInput * movementSpeed * Time.deltaTime
 
     void OnCollisionEnter(Collision col){
-        Debug.Log("Player colidiu com "+col);
-        if(col.gameObject.tag=="treadmill"){
+        Debug.Log("Player colidiu com "+col.gameObject.tag);
+        if(col.gameObject.tag=="treadmill" && timer < Time.time-jumpLenght){
             anim.SetBool("isJumping",false);
         }
     }
