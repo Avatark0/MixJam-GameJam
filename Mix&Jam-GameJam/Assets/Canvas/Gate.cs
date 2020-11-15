eef72370246dc749f2a6a5c7db6a52f631f590cd
Rafael Caseiro <43393257+Avatark0@users.noreply.github.com>
2020-11-15 06:01:24 -0300

    Major Update
    pt 1

---
+++ Mix&Jam-GameJam/Assets/Canvas/Gate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col){
+        Debug.Log("BoomBarrier colidiu com "+col.gameObject.tag);
+        if(col.gameObject.tag=="wallLeft"){
+            Destroy(GetComponent<HingeJoint>());
+        }
+    }
+
+    void OnTriggerEnter(Collider col){
+        Debug.Log("BoomBarrier trigou em "+col.gameObject.tag);
+    }
+}
